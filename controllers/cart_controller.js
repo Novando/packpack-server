31298ce101a4a72e60ccb59b6754b04507f44fbb
novando <rizkynovando@gmail.com>
2022-06-14 11:19:57 +0700

    fix bug

--- controllers/cart_controller.js
+++ controllers/cart_controller.js
@@ -108,7 +108,7 @@ exports.showUser = async (req, res) => {
 					}
 				})
 				// await Promise.all();
-				const subPrice = () = parseFloat(item.length) * parseFloat(item.qty) * parseFloat(getMaterial.price) || 0
+				const subPrice = parseFloat(item.length) * parseFloat(item.qty) * parseFloat(getMaterial.price) || 0
 				const subWeight = parseFloat(item.length) * parseFloat(item.qty) * parseFloat(getMaterial.weight) || 0
 				
 				let theProduct = Object.assign(
